d54b7b626fc378a1aebfc5858e6d7f659369a839
TokcDK <TokcDK@gmail.com>
2026-09-25 00:38:25 +0500

    add core project

---
+++ RGSS-Extractor.Core/Entry.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace RGSS_Extractor
+{
+	public class Entry
+	{
+		public string Name;
+
+		public long Offset;
+
+		public long Size;
+
+		public int Datakey;
+
+		public byte[] Data;
+	}
+}
